89b9d44712463402dd209bbf9c7bfe5b25df4d9d
svetlanaryzhkova <sveta.ryzhkova.00@bk.ru>
2023-06-20 16:57:00 +0400

    добавила интеграционных тестов

--- OrderProcessing.IntegrationTests/OrderProcessingAppTests.cs
+++ OrderProcessing.IntegrationTests/OrderProcessingAppTests.cs
@@ -34,33 +34,46 @@ namespace OrderProcessing.IntegrationTests
                 }
             }
         }
-
-        public void Run_IncorrectValues_CorrectDataInOutputFile()
+        
+        [TestMethod]
+        public void Run_IncorrectInputPath_ThrowsDirectoryException()
         {
-            string cake = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\Orders\\Cake.csv";
-            string icecream = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\Orders\\IceCream.csv";
+            string cake = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\NotFound\\Cake.csv";
             string output = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\Output.txt";
-            string[,] productsCake = { { "sugar", "2" }, { "milk", "4" }, { "eggs", "10" } };
-            string[,] productsIcecream = { { "sugar", "1" }, { "chocolate", "12" }, { "milk", "11" } };
-            CSVFileGenerator generator = new CSVFileGenerator();
-            generator.createCSVFile(cake, productsCake);
-            generator.createCSVFile(icecream, productsIcecream);
-            string[] args = { cake, icecream, "-o", output };
+            string[] args = { cake, "-o", output };
             FileReader fileReader = new FileReader();
             FileWriter fileWriter = new FileWriter();
             OrderProcessingApp app = new OrderProcessingApp(fileWriter, fileReader);
-            string[] result = { "Product,Quantity", "sugar,3", "milk,15", "eggs,10", "chocolate,12" };
 
-            app.Run(args);
+            Assert.ThrowsException<DirectoryNotFoundException>(() => app.Run(args));
+        }
 
-            using (StreamReader sr = new StreamReader(output))
-            {
-                for (int i = 0; i < result.Length; i++)
-                {
-                    string? line = sr.ReadLine();
-                    StringAssert.Contains(line, result[i]);
-                }
-            }
+        [TestMethod]
+        public void Run_IncorrectOutputPath_ThrowsDirectoryException()
+        {
+            string cake = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\Orders\\Cake.csv";
+            string output = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\NotFound\\Output.txt";
+            string[] args = { cake, "-o", output };
+            FileReader fileReader = new FileReader();
+            FileWriter fileWriter = new FileWriter();
+            OrderProcessingApp app = new OrderProcessingApp(fileWriter, fileReader);
+
+            Assert.ThrowsException<DirectoryNotFoundException>(() => app.Run(args));
+        }
+
+        [TestMethod]
+        public void Run_IncorrectParameters_ReturnZero()
+        {
+            string cake = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\Orders\\Cake.csv";
+            string output = "C:\\Users\\svetl\\Documents\\MyProjects\\OrderProcessing\\OrderProcessing.IntegrationTests\\Output.txt";
+            string[] args = { cake, output };
+            FileReader fileReader = new FileReader();
+            FileWriter fileWriter = new FileWriter();
+            OrderProcessingApp app = new OrderProcessingApp(fileWriter, fileReader);
+
+            int stateResult = app.Run(args);
+
+            Assert.AreEqual(0, stateResult);
         }
     }
 }
--- OrderProcessing/OrderProcessingApp.cs
+++ OrderProcessing/OrderProcessingApp.cs
@@ -17,15 +17,18 @@ namespace OrderProcessing
             this.fileReader = fileReader;
         }
 
-        public void Run(string[] args)
+        public int Run(string[] args)
         {
             var opts = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            if (opts.Value == null) return 0;
             IEnumerable<string> inputPathes = opts.Value.InputFiles;
             string outputPath = opts.Value.OutputFile;
 
             Dictionary<string, int> order = fileReader.readFiles(inputPathes);
 
             fileWriter.writeInFile(outputPath, order);
+
+            return 0;
         }
     }
 }
